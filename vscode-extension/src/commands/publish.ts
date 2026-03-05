df35809b2841da21d3d555ecee8a5968c57a6e6d
huanent <huanent@icloud.com>
2026-03-05 09:49:17 +0800

    Update publish command logic and modify context menu conditions

--- vscode-extension/src/commands/publish.ts
+++ vscode-extension/src/commands/publish.ts
@@ -1,22 +1,9 @@
 import * as vscode from 'vscode';
-import fs from "fs";
-import path from 'path';
 
 const terminalName = 'dotnet-deploy';
 
 export function registerPublishCommand(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand('dotnet-deploy.publish', async (uri: vscode.Uri) => {
-        const files = fs.readdirSync(uri.path, {
-            recursive: true,
-            withFileTypes: true
-        });
-
-        const csprojFiles = files
-            .filter(f => f.isFile() && f.name.endsWith('.csproj'))
-            .map(f => path.join(f.parentPath, f.name));
-
-        if (!csprojFiles.length) return;
-
         let terminal = vscode.window.terminals.find(f => f.name === terminalName);
 
         if (terminal) {
@@ -25,7 +12,7 @@ export function registerPublishCommand(context: vscode.ExtensionContext) {
 
         terminal = vscode.window.createTerminal(terminalName);
         terminal.show();
-        const command = ["dotnet", "deploy", "publish", "--project", csprojFiles[0]];
+        const command = ["dotnet", "deploy", "publish", "--project", uri.path];
         terminal.sendText(command.join(' '));
     });
 
